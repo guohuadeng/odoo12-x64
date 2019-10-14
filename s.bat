07a762e2599371a86836ceaa5093e1e33b85f4f8
Ivan Home <guohuadeng@hotmail.com>
2019-10-15 03:03:24 +0800

    update for empty dir

--- s.bat
+++ s.bat
@@ -1,7 +1,9 @@
-title GreenOdoo12 x64 fast - www.sunpop.cn
+title GreenOdoo13 x64 fast - www.sunpop.cn
 %CD%\runtime\bin\pv.exe -f -k python.exe -q
 %CD%\runtime\bin\pv.exe -f -k python3.exe -q
 %CD%\runtime\pgsql\bin\pg_ctl stop -D %CD%\runtime\pgsql\data -s -m fast
+%CD%\runtime\pgsql\bin\pg_ctl stop -D %CD%\zdata\pg -s -m fast
+%CD%\runtime\pgsql\bin\pg_ctl stop -D d:\pjweb\o12.odooapp.cn\z\pg -s -m fast
 %CD%\runtime\bin\pv.exe -f -k postgres.exe -q
 tskill postgres
 tskill python
